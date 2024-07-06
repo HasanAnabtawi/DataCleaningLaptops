3cfd5941160222aef884e66ba38817a471c9fddb
HasanAnabtawi <h-anabtawi@hotmail.com>
2024-07-06 12:54:20 +0300

    Adding_columns

--- Data Cleaning Project.sql
+++ Data Cleaning Project.sql
@@ -24,17 +24,48 @@ where [Id]='';
 Delete From [Portfolio project].[dbo].[laptopData]
 where [Company]='MSI';
 
+
+
+
 --Cpu Manufacturer Company splitting
-Select SUBSTRING(Cpu,1,CHARINDEX(' ',Cpu,1)) Cpu_Manufacturer From [Portfolio project].[dbo].[laptopData]
+
+
+
+Alter Table [Portfolio project].[dbo].[laptopData]
+Add Cpu_Manufacturer Varchar(50);
+
+update [Portfolio project].[dbo].[laptopData]
+set Cpu_Manufacturer=SUBSTRING(Cpu,1,CHARINDEX(' ',Cpu,1));
+
 
 -- Cpu Model splitting
-Select SUBSTRING(Cpu,CHARINDEX(' ',Cpu,1),LEN(Cpu)) Cpu_Model  From [Portfolio project].[dbo].[laptopData]
+
+Alter Table [Portfolio project].[dbo].[laptopData]
+Add Cpu_Model Varchar(50);
+
+
+update [Portfolio project].[dbo].[laptopData]
+set Cpu_Model=SUBSTRING(Cpu,CHARINDEX(' ',Cpu,1),LEN(Cpu));
+
 
 --Gpu Manufacturer Company splitting
-Select SUBSTRING(Gpu,1,CHARINDEX(' ',Gpu,1)) Gpu_Manufacturer From [Portfolio project].[dbo].[laptopData]
+
+Alter Table [Portfolio project].[dbo].[laptopData]
+Add Gpu_Manufacturer Varchar(50);
+
+update [Portfolio project].[dbo].[laptopData]
+set Gpu_Manufacturer=SUBSTRING(Gpu,1,CHARINDEX(' ',Gpu,1));
+
 
 -- Cpu Model splitting
-Select SUBSTRING(Gpu,CHARINDEX(' ',Gpu,1),LEN(Gpu)) Gpu_Model  From [Portfolio project].[dbo].[laptopData]
+
+Alter Table [Portfolio project].[dbo].[laptopData]
+Add Gpu_Model Varchar(50);
+
+update [Portfolio project].[dbo].[laptopData]
+set Gpu_Model=SUBSTRING(Gpu,CHARINDEX(' ',Gpu,1),LEN(Gpu));
+
+
 
 
 
